e72e7d531dfd3171024ec1c8342a2211484f1e4f
anilyavas <anil.yavas99@gmail.com>
2025-04-24 22:18:19 +0300

    added map view and latest earthquake location display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,9 @@ import {
   StatusBar,
   ActivityIndicator,
   ScrollView,
+  Dimensions,
 } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 import { fetchEarthquake } from '~/utils/fetchEarthquake';
 import { getLocation } from '~/utils/getLocation';
@@ -22,6 +24,7 @@ export default function Home() {
   } | null>(null);
   const [loading, setLoading] = useState(true);
   const [earthquakes, setEarthquakes] = useState<any[]>([]);
+  const [latestQuakeLocation, setLatestQuakeLocation] = useState();
 
   useEffect(() => {
     const fetchLocation = async () => {
@@ -33,6 +36,9 @@ export default function Home() {
           longitude: coords.longitude,
         });
         setEarthquakes(results);
+        const sorted = results.sort((a: any, b: any) => b.properties.time - a.properties.time);
+        const latestQuake = sorted[0];
+        setLatestQuakeLocation(latestQuake);
       }
       setLoading(false);
     };
@@ -44,36 +50,70 @@ export default function Home() {
       <SafeAreaView
         className="flex-1"
         style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }}>
-        {loading ? (
-          <ActivityIndicator size="large" />
-        ) : location ? (
-          <View className="p-2">
-            <View className="pb-5">
-              <Text className="text-center font-semibold">
-                Location: {location.city},{location.region}/{location.country}
-              </Text>
-            </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {loading ? (
+            <ActivityIndicator size="large" />
+          ) : location ? (
             <View className="p-2">
-              {earthquakes.length > 0 && (
-                <ScrollView className="mb-8 mt-4 px-2" showsVerticalScrollIndicator={false}>
-                  <Text className="mb-2 text-lg font-semibold">Nearby Earthquakes</Text>
-                  {earthquakes.map((quake) => (
-                    <View key={quake.id} className="mb-2 rounded-lg bg-white p-3 shadow">
-                      <Text className="font-medium text-red-600">
-                        M{quake.properties.mag} - {quake.properties.place}
-                      </Text>
-                      <Text className="text-xs text-gray-500">
-                        {new Date(quake.properties.time).toLocaleString()}
-                      </Text>
-                    </View>
-                  ))}
-                </ScrollView>
-              )}
+              <View className="pb-5">
+                <Text className="text-center font-semibold">
+                  Location: {location.city},{location.region}/{location.country}
+                </Text>
+              </View>
+              <View className="h-96 p-2">
+                <Text className="mb-2 text-lg font-semibold">Nearby Earthquakes</Text>
+                {earthquakes.length > 0 && (
+                  <ScrollView
+                    contentContainerClassName="gap-2"
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 100 }}>
+                    {earthquakes.map((quake) => (
+                      <View key={quake.id} className="mb-2 rounded-lg bg-white p-3 shadow">
+                        <Text className="font-medium text-red-600">
+                          {quake.properties.mag} - {quake.properties.place}
+                        </Text>
+                        <Text className="text-xs text-gray-500">
+                          {new Date(quake.properties.time).toLocaleString()}
+                        </Text>
+                      </View>
+                    ))}
+                  </ScrollView>
+                )}
+              </View>
+              <View className="p-2">
+                <Text className="mb-2 text-lg font-semibold">Latest Earthquake</Text>
+                <MapView
+                  style={{
+                    width: Dimensions.get('window').width / 1.1,
+                    height: Dimensions.get('window').height / 3.25,
+                    flex: 1,
+                    alignSelf: 'center',
+                  }}
+                  mapType="hybrid"
+                  showsUserLocation
+                  initialRegion={{
+                    latitude: latestQuakeLocation?.geometry.coordinates[1] || location.latitude,
+                    longitude: latestQuakeLocation?.geometry.coordinates[0] || location.longitude,
+                    latitudeDelta: 2,
+                    longitudeDelta: 2,
+                  }}>
+                  {latestQuakeLocation && (
+                    <Marker
+                      coordinate={{
+                        latitude: latestQuakeLocation.geometry.coordinates[1],
+                        longitude: latestQuakeLocation.geometry.coordinates[0],
+                      }}
+                      title={`Magnitude of ${latestQuakeLocation.properties.mag} Earthquake`}
+                      description={latestQuakeLocation.properties.place}
+                    />
+                  )}
+                </MapView>
+              </View>
             </View>
-          </View>
-        ) : (
-          <Text>Failed to get location.</Text>
-        )}
+          ) : (
+            <Text>Failed to get location.</Text>
+          )}
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
