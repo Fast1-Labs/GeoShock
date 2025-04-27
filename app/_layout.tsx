ddfeacd39e1ec3c4ca64604353066a234f0dcbd4
anilyavas <anil.yavas99@gmail.com>
2025-04-27 20:18:07 +0300

    onboarding and dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="(onborading)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
